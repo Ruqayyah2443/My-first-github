0c67feedafaf65b6dc0c63c6b88e15815e05f4cc
Ruqayyah <ruqayyahadebanjo22@gmail.com>
2025-09-07 13:15:39 +0100

    assignment on operators and variables

---
+++ week1/javascript.js
@@ -0,0 +1,20 @@
+// const name = prompt("What is your name?")
+// console.log(name)
+
+// let jobTitle = prompt("What is your job role?")
+// console.log(jobTitle)
+
+
+let num1 = 10;
+let num2 = 8;
+let name = "Ruqayyah";
+let result = num1 + num2;
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log("Hello " + name + "," + " the result of " +  num1 + " +" + num2 + " is " + result);
+
+console.log(`Hello ${name}, the result of ${num1} + ${num2} is ${result}`)
+   
